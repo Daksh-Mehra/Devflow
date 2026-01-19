4e2fb36181d37508d54e351e3548a086a1bdc7df
Daksh-Mehra <daksh.m2003bv@gmail.com>
2026-01-19 15:24:07 +0530

    Implement RightSidebar

--- constants/routes.ts
+++ constants/routes.ts
@@ -1,7 +1,9 @@
 const ROUTES={
     HOME:"/",
     SIGN_IN:"/sign-in",
-    SIGN_UP:"/sign-up"
+    SIGN_UP:"/sign-up",
+    PROFILE:(id:string)=>`/profile/${id}`,
+    TAGS:(_id:string)=>`/tags/${_id}`,
 };
 
 export default ROUTES
