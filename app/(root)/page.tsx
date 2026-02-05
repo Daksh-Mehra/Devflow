212bdd65b866ca103bec02ed543ab2305d384033
Daksh-Mehra <daksh.m2003bv@gmail.com>
2026-02-05 14:56:37 +0530

    Implement Home page Layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,28 @@
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import Link from "next/link";
 
 
 const Home =async () => {
   
   return<>
-   <div className="text-2xl min-h-screen">welcome to my website</div> 
+  <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+
+   <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+   <Button className="primary-gradient min-h-[46px] !text-light-900  px-4 py-3 " asChild>
+    <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+    </Button> 
+  </section>
+
+  <section className="mt-11">LocalSearch
+  </section>
+  HomeFilter
+  <div className="mt-10 flex w-full flex-col gap-6">
+    <p>Question card 1</p>
+    <p>Question card 1</p>
+    <p>Question card 1</p>
+    <p>Question card 1</p>
+  </div>
   </>
 }
 
